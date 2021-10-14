ed039d991c82c694ade61e122b55bd27b294fb0c
NullPopPo <sylph@reset.jp>
2021-10-14 15:32:02 +0000

    操作こんな感じデモ一旦完成

---
+++ Assets/Editor/Test/Rot2DTest.cs
@@ -0,0 +1,110 @@
+﻿/*!	@file
+	@brief PluggableVR: Rot2D 単体テスト 
+	@author NullPopPoLab
+	@sa https://github.com/NullPopPoLab/PluggableVR_Unity
+*/
+using UnityEngine;
+using NUnit.Framework;
+
+namespace PluggableVR
+{
+
+	public class Rot2DTest : TestCommon
+	{
+
+		[Test]
+		public void FromCycle()
+		{
+
+			// 回転量と度のインスタンス生成比較 
+			for (var d = -1080; d <= 1080; ++d)
+			{
+				var r = Cycle.FromDegree(d);
+				var t1 = Rot2D.FromCycle(r);
+				var t2 = Rot2D.FromDegree(d);
+				CompareLoose(t1.C, t2.C, "Rot2D.FromCycle(" + d + " deg).C");
+				CompareLoose(t1.S, t2.S, "Rot2D.FromCycle(" + d + " deg).S");
+			}
+
+			// 直角から生成されるインスタンスで端数が出ないことを保証 
+			Compare(Rot2D.FromCycle(Cycle.Right * -2).C, -1, "Rot2D.FromCycle(-2r).C");
+			Compare(Rot2D.FromCycle(Cycle.Right * -2).S, 0, "Rot2D.FromCycle(-2r).S");
+			Compare(Rot2D.FromCycle(Cycle.Right * -1).C, 0, "Rot2D.FromCycle(-1r).C");
+			Compare(Rot2D.FromCycle(Cycle.Right * -1).S, -1, "Rot2D.FromCycle(-1r).S");
+			Compare(Rot2D.FromCycle(Cycle.Right * 0).C, 1, "Rot2D.FromCycle(0r).C");
+			Compare(Rot2D.FromCycle(Cycle.Right * 0).S, 0, "Rot2D.FromCycle(0r).S");
+			Compare(Rot2D.FromCycle(Cycle.Right * 1).C, 0, "Rot2D.FromCycle(1r).C");
+			Compare(Rot2D.FromCycle(Cycle.Right * 1).S, 1, "Rot2D.FromCycle(1r).S");
+			Compare(Rot2D.FromCycle(Cycle.Right * 2).C, -1, "Rot2D.FromCycle(2r).C");
+			Compare(Rot2D.FromCycle(Cycle.Right * 2).S, 0, "Rot2D.FromCycle(2r).S");
+			Compare(Rot2D.FromCycle(Cycle.Right * 3).C, 0, "Rot2D.FromCycle(3r).C");
+			Compare(Rot2D.FromCycle(Cycle.Right * 3).S, -1, "Rot2D.FromCycle(3r).S");
+			Compare(Rot2D.FromCycle(Cycle.Right * 4).C, 1, "Rot2D.FromCycle(4r).C");
+			Compare(Rot2D.FromCycle(Cycle.Right * 4).S, 0, "Rot2D.FromCycle(4r).S");
+		}
+
+		[Test]
+		public void FromDegree()
+		{
+
+			// 度からのインスタンス生成 
+			for (var d = -360; d <= 360; ++d)
+			{
+				var r = Mathf.Deg2Rad * d;
+				var c = Mathf.Cos(r);
+				var s = Mathf.Sin(r);
+				var t = Rot2D.FromDegree(d);
+				CompareLoose(t.C, c, "Rot2D.FromDegree(" + d + " deg).C");
+				CompareLoose(t.S, s, "Rot2D.FromDegree(" + d + " deg).S");
+			}
+		}
+
+		[Test]
+		public void FromCosine()
+		{
+
+			// コサイン値からのインスタンス生成 
+			for (var d = 0; d <= 180; ++d)
+			{
+				var r = Mathf.Deg2Rad * d;
+				var c = Mathf.Cos(r);
+				var s = Mathf.Sin(r);
+				var t = Rot2D.FromCosine(c);
+				CompareLoose(t.C, c, "Rot2D.FromCosine(" + d + " deg).S");
+				CompareLoose(t.S, s, "Rot2D.FromCosine(" + d + " deg).S");
+			}
+		}
+
+		[Test]
+		public void Double()
+		{
+
+			// 倍角 
+			for (var d = -360; d <= 360; ++d)
+			{
+				var t1 = Rot2D.FromDegree(d * 2);
+				var t2 = Rot2D.FromDegree(d).Double;
+				CompareLoose(t1.C, t2.C, "Rot2D(" + d + " deg).Double.C");
+				CompareLoose(t1.S, t2.S, "Rot2D(" + d + " deg).Double.S");
+			}
+		}
+
+		[Test]
+		public void Half()
+		{
+
+			// 半角 
+			// (注) ±180°を超えると、半分にしてどちらに寄せるか問題
+			//		により結果がややこしいことになる。
+			// (注) ±180°ぴったりのつもりでも、微妙な計算誤差で
+			//		結果が二分されるため、これまたややこしい。
+			for (var d = -179; d < 180; ++d)
+			{
+				var t1 = Rot2D.FromDegree(d * 0.5f);
+				var t2 = Rot2D.FromDegree(d).Half;
+				CompareLoose(t1.C, t2.C, "Rot2D(" + d + " deg).Half.C");
+				CompareLoose(t1.S, t2.S, "Rot2D(" + d + " deg).Half.S");
+			}
+		}
+	}
+}
---
+++ Assets/Editor/Test/RotUtTest.cs
@@ -0,0 +1,133 @@
+﻿/*!	@file
+	@brief PluggableVR: RotUt 単体テスト 
+	@author NullPopPoLab
+	@sa https://github.com/NullPopPoLab/PluggableVR_Unity
+*/
+using UnityEngine;
+using NUnit.Framework;
+
+namespace PluggableVR
+{
+
+	public class RotUtTest : TestCommon
+	{
+
+		private Quaternion _testSubj
+		{
+			get
+			{
+				return Quaternion.Euler(50, 100, 200);
+			}
+		}
+
+		[Test]
+		public void Axis()
+		{
+
+			// 回転軸の向き計算 
+			var q = _testSubj;
+			CompareLoose(RotUt.AxisX(q), q * new Vector3(1, 0, 0), "AxisX");
+			CompareLoose(RotUt.AxisY(q), q * new Vector3(0, 1, 0), "AxisY");
+			CompareLoose(RotUt.AxisZ(q), q * new Vector3(0, 0, 1), "AxisZ");
+		}
+
+		[Test]
+		public void RotX()
+		{
+
+			// X軸回転計算 
+			for (var d = -360; d < 360; ++d)
+			{
+				var r = Mathf.PI / 180.0f * d;
+				var q1 = RotUt.RotX(r);
+				var q2 = RotUt.RotX(Rot2D.FromDegree(d));
+				CompareLoose(q1, q2, "RotX(" + d + ")");
+			}
+		}
+
+		[Test]
+		public void RotY()
+		{
+
+			// Y軸回転計算 
+			for (var d = -360; d < 360; ++d)
+			{
+				var r = Mathf.PI / 180.0f * d;
+				var q1 = RotUt.RotY(r);
+				var q2 = RotUt.RotY(Rot2D.FromDegree(d));
+				CompareLoose(q1, q2, "RotY(" + d + ")");
+			}
+		}
+
+		[Test]
+		public void RotZ()
+		{
+
+			// Z軸回転計算 
+			for (var d = -360; d < 360; ++d)
+			{
+				var r = Mathf.PI / 180.0f * d;
+				var q1 = RotUt.RotZ(r);
+				var q2 = RotUt.RotZ(Rot2D.FromDegree(d));
+				CompareLoose(q1, q2, "RotZ(" + d + ")");
+			}
+		}
+
+		[Test]
+		public void RotAny()
+		{
+
+			var q = _testSubj;
+
+			// 任意軸回転計算 
+			for (var d = -360; d < 360; ++d)
+			{
+				var r = Mathf.PI / 180.0f * d;
+				var q1 = RotUt.Rot(RotUt.AxisX(q), r);
+				var q2 = RotUt.Rot(RotUt.AxisX(q), Rot2D.FromDegree(d));
+				CompareLoose(q1, q2, "RotAnyX(" + d + ")");
+
+				q1 = RotUt.Rot(RotUt.AxisY(q), r);
+				q2 = RotUt.Rot(RotUt.AxisY(q), Rot2D.FromDegree(d));
+				CompareLoose(q1, q2, "RotAnyY(" + d + ")");
+
+				q1 = RotUt.Rot(RotUt.AxisZ(q), r);
+				q2 = RotUt.Rot(RotUt.AxisZ(q), Rot2D.FromDegree(d));
+				CompareLoose(q1, q2, "RotAnyZ(" + d + ")");
+			}
+		}
+
+		[Test]
+		public void ReturnX()
+		{
+
+			var q1 = _testSubj;
+			var q2 = RotUt.ReturnX(q1);
+
+			// X軸を元に戻す 
+			CompareLoose(RotUt.AxisX(q2), new Vector3(1, 0, 0), "ReturnX");
+		}
+
+		[Test]
+		public void ReturnY()
+		{
+
+			var q1 = _testSubj;
+			var q2 = RotUt.ReturnY(q1);
+
+			// Y軸を元に戻す 
+			CompareLoose(RotUt.AxisY(q2), new Vector3(0, 1, 0), "ReturnY");
+		}
+
+		[Test]
+		public void ReturnZ()
+		{
+
+			var q1 = _testSubj;
+			var q2 = RotUt.ReturnZ(q1);
+
+			// Z軸を元に戻す 
+			CompareLoose(RotUt.AxisZ(q2), new Vector3(0, 0, 1), "ReturnZ");
+		}
+	}
+}
