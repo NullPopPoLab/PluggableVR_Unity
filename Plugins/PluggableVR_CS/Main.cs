22cda07c14261a079b0044869e4332846e2f3bc5
NullPopPo <sylph@reset.jp>
2021-10-16 13:21:51 +0000

    プラグイン実験

---
+++ Plugins/PluggableVR_CS/Main.cs
@@ -0,0 +1,36 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using Studio;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace PluggableVR_CS
+{
+    [BepInPlugin(GUID, "PluggableVR for CharaStudio", VERSION)]
+    [BepInProcess("CharaStudio")]
+    public class Main : BaseUnityPlugin
+    {
+        private const string GUID = "com.nullpoppo.PluggableVR.CS";
+        public const string VERSION = "0.0.0.1";
+
+        protected void Awake()
+        {
+            Harmony.CreateAndPatchAll(typeof(Main));
+        }
+
+        protected void OnEnable()
+        {
+            SceneManager.sceneLoaded += _onSceneChanged;
+        }
+
+        protected void OnDisable()
+        {
+            SceneManager.sceneLoaded -= _onSceneChanged;
+        }
+
+        private void _onSceneChanged(Scene scn, LoadSceneMode mode)
+        {
+		}
+    }
+}
