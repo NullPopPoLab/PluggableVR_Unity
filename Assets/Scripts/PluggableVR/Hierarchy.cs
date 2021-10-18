4d4f5383b4af02dc46c0a500d1261da50167e8ca
NullPopPo <sylph@reset.jp>
2021-10-18 10:30:18 +0000

    PluggableVR.Hierarchy.Dump2File() でHierarchy内容をファイルに書き出す

--- Assets/Scripts/PluggableVR/Hierarchy.cs
+++ Assets/Scripts/PluggableVR/Hierarchy.cs
@@ -4,6 +4,8 @@
 	@sa https://github.com/NullPopPoLab/PluggableVR_Unity
 */
 using System;
+using System.IO;
+using System.Text;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -41,6 +43,104 @@ namespace PluggableVR
 				if (t.parent != null) continue;
 				_dumpChildren(obj, a, cb);
 			}
+		}
+
+		private static string _fmtFloat(float v)
+		{
+			var s = (v < 0.0f);
+			if (s) v = -v;
+			return (s ? "-" : "+") + v.ToString("F3");
+		}
+
+		private static string _fmtVector(Vector3 v)
+		{
+			return "(" + _fmtFloat(v.x) + "," + _fmtFloat(v.y) + "," + _fmtFloat(v.z) + ")";
+		}
+
+		private static void _dump2File(FileStream fs, GameObject obj, IList<GameObject> anc)
+		{
+
+			var il = anc.Count;
+			var s = "";
+
+			var t1 = "";
+			var t2 = "";
+			var t3 = "";
+			var t4 = "";
+			var t5 = "";
+			var t6 = "";
+			for (var i = 0; i < il - 2; ++i) t1 += " |  ";
+			t2 = t1;
+			if (il > 0)
+			{
+				t1 += " +--";
+				t2 += " |  ";
+			}
+			t6 = t5 = t4 = t3 = t2;
+			t2 += " | ";
+			t3 += " |~";
+			t4 += " |  ; ";
+			t5 += " |";
+			t6 += " `";
+
+			s += t1 + (obj.activeSelf ? "[*]" : "[_]");
+			if (obj.isStatic) s += "[S]";
+			s += " " + obj.name;
+			s += "\n";
+
+			s += t2 + "Scene: " + obj.scene.name + "\n";
+			s += t2 + "InstanceID: " + obj.GetInstanceID() + "\n";
+			s += t2 + "Layer: " + obj.layer + "\n";
+			s += t2 + "Tag: " + obj.tag + "\n";
+
+			var tr = obj.transform;
+			s += t2 + "Pos: " + _fmtVector(tr.position) + "\n";
+			var r = tr.rotation;
+			s += t2 + "RtX: " + _fmtVector(RotUt.AxisX(r)) + "\n";
+			s += t2 + "RtY: " + _fmtVector(RotUt.AxisY(r)) + "\n";
+			s += t2 + "RtZ: " + _fmtVector(RotUt.AxisZ(r)) + "\n";
+
+			var cs = obj.GetComponents(typeof(Component));
+			il = cs.Length;
+			for (var i = 0; i < il; ++i)
+			{
+				var c = cs[i];
+				var b = c as Behaviour;
+				s += t3;
+				if (b == null) s += "{@} ";
+				else s += b.enabled ? "{*} " : "{_} ";
+
+				var cn = c.ToString();
+				var ol = obj.name.Length;
+				if (cn.Substring(cn.Length - 1) != ")") { }
+				else if (cn.Substring(0, ol + 2) != obj.name + " (") { }
+				else cn = cn.Substring(ol + 2, cn.Length - ol - 3);
+				s += cn + "\n";
+
+				s += t4 + "InstanceID: " + c.GetInstanceID() + "\n";
+			}
+
+			if (tr.childCount < 1) s += t6 + "\n";
+
+			var bin = new UTF8Encoding(true).GetBytes(s);
+			fs.Write(bin, 0, bin.Length);
+		}
+
+		//! GameObject 全列挙してファイルに書き出す 
+		public static void Dump2File(string prefix)
+		{
+
+			string path;
+			do
+			{
+				path = prefix + DateTime.Now.ToString("_yyymmdd_HHmmss_fff") + ".log";
+			} while (File.Exists(path));
+
+			var fs = new FileStream(path, FileMode.Create);
+			fs.Close();
+			fs = new FileStream(path, FileMode.Truncate);
+			Dump((obj, anc) => _dump2File(fs, obj, anc));
+			fs.Close();
 		}
 	}
 }
